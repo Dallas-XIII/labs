77071ac3713ec5dd5b8ca741676dbfa96de50947
MetallX <9malcev@gmail.com>
2021-11-18 12:31:01 +0300

    +for.py

---
+++ for.py
@@ -0,0 +1,12 @@
+for i in range(1, 11):
+    if i == 5:
+        pass 
+    print('Cycle iteration #',i) 
+else:
+    print('Cycle end, 5th iteration is None')
+it = 0    
+for i in ('asdfgh'):
+    print(i)
+    it = it + 1
+else:
+    print('Cycle end with',it, 'iterations')
